6d8dabcf79ad9f8faeb1ce30758bb32bb05bb48e
CMejiaVergel <dev@voltac.com>
2026-04-09 18:27:37 -0500

    fix(config): estabilizar configuracion del limite de tamano NextJs 15+ a 50mb

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['sharp'],
+  serverActions: {
+    bodySizeLimit: '50mb',
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: '50mb',
